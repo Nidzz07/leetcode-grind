68050195432753d6b60fc324904df9f75d19a06c
Nidhi Dhyani <nsdhyani22@gmail.com>
2026-06-29 16:57:12 +0530

    Time: 0 ms (100%), Space: 42.8 MB (83.56%) - LeetHub

--- 0058-length-of-last-word/0058-length-of-last-word.java
+++ 0058-length-of-last-word/0058-length-of-last-word.java
@@ -1,6 +1,17 @@
 class Solution {
     public int lengthOfLastWord(String s) {
-        String []arr=s.split(" ");
-        return arr[arr.length-1].length();
+        int length = 0;
+        int i = s.length() - 1;
+
+        while (i >= 0 && s.charAt(i) == ' ') {
+            i--;
+        }
+
+        while (i >= 0 && s.charAt(i) != ' ') {
+            length++;
+            i--;
+        }
+
+        return length;
     }
 }
